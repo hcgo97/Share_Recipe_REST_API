2e265553c6388f2932b58c6832b0bf7417a91c39
hcgo97 <hcgo_o@naver.com>
2021-02-24 00:46:12 +0900

    ✨ 구조개선 - dto 추가

--- src/main/java/com/hyoj/share_recipe/dto/BoardWriteDto.java
+++ src/main/java/com/hyoj/share_recipe/dto/BoardWriteDto.java
@@ -1,47 +1,38 @@
-package com.hyoj.share_recipe.entity;
+package com.hyoj.share_recipe.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import org.hibernate.annotations.ColumnDefault;
 
-import javax.persistence.*;
 import javax.validation.constraints.NotNull;
-import java.sql.Date;
+import java.time.LocalDateTime;
 
 @Data
-@Entity(name = "board_tbl")
-public class BoardEntity {
+public class BoardWriteDto {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @NotNull(message = "잘못된 요청입니다.")
+    @JsonIgnore
     private int unq;
 
-    @Column
     @NotNull(message = "로그인 후 글 작성이 가능합니다.")
     private String userid;
 
-    @Column
     @NotNull(message = "제목을 입력해주세요.")
     private String title;
 
-    @Column
     @NotNull(message = "카테고리를 선택해주세요.")
     private String category;
 
-    @Column
     @NotNull(message = "재료를 선택해주세요.")
     private String mat;
 
-    @Column
     @NotNull(message = "내용을 입력해주세요.")
     private String recipe;
 
-    @Column
     @ColumnDefault("0")
     private int hits;
 
-    @Column
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Date rdate;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    private LocalDateTime rdate;
 
 }
